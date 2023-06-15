6d2f6ac5ba93a756d1094bd5302e41626806b0ae
WeslleiBrito <wesllei.wbs@gmail.com>
2023-06-14 22:08:32 -0300

    Aula finalizada

--- src/index.ts
+++ src/index.ts
@@ -21,42 +21,164 @@ app.get("/accounts", (req: Request, res: Response) => {
 })
 
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
 
-    const result = accounts.find((account) => account.id === id) 
+    try {
+        const regexIDID = /^a\d{3}$/
+        const id = req.params.id
 
-    res.status(200).send(result)
+        if(!regexIDID.test(id)){
+            res.status(400)
+            throw new Error("Id inválido, o id deve ter a seguinte estrutura: 'a001', 'a123', 'a999'.")
+        }
+
+        const result = accounts.find((account) => account.id === id) 
+
+        if(!result){
+            res.statusCode = 404
+            throw new Error("Conta não encontrada! Verifique o Id.")
+        }
+    
+        res.status(200).send(result)
+
+    } catch (error) {
+
+        if(error instanceof Error){
+            res.send(error.message)
+        }
+
+        res.status(500).send("Erro desconheicido!")
+        
+    }
+   
 })
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try {
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+        const regexID = /^a\d{3}$/
+        const id = req.params.id
 
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
+        if(!regexID.test(id)){
+            res.status(400)
+            throw new Error("Id inválido, o id deve ter a seguinte estrutura: 'a001', 'a123', 'a999'.")
+        }
+
+        const result = accounts.findIndex((account) => account.id === id) 
+        console.log(result)
+
+        if(result < 0){
+            res.statusCode = 404
+            throw new Error("Conta não encontrada! Verifique o Id.")
+        }
+
+        accounts.splice(result, 1)
+        
+        res.status(200).send("Item deletado com sucesso")
+
+    } catch (error) {
+
+        if(error instanceof Error){
+            res.send(error.message)
+        }
+
+        res.status(500).send("Erro desconheicido!")
+        
     }
-
-    res.status(200).send("Item deletado com sucesso")
+    
 })
 
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+    try {
 
-    const account = accounts.find((account) => account.id === id) 
+        const regexID = /^a\d{3}$/
+        const regexOwnerName = /^.{2,}$/
 
-    if (account) {
-        account.id = newId || account.id
-        account.ownerName = newOwnerName || account.ownerName
-        account.type = newType || account.type
+        const id = req.params.id
 
-        account.balance = isNaN(newBalance) ? account.balance : newBalance
+        if(!regexID.test(id)){
+            res.status(400)
+            throw new Error("Id inválido, o id deve ter a seguinte estrutura: 'a001', 'a123', 'a999'.")
+        }
+
+        const result = accounts.find((account) => account.id === id) 
+
+        if(!result){
+            res.statusCode = 404
+            throw new Error("Conta não encontrada! Verifique o Id.")
+        }
+
+        const newId = req.body.id
+        const newOwnerName = req.body.ownerName
+        const newBalance = req.body.balance
+        const newType = req.body.type
+
+        if(newId){
+
+            if(typeof(newId) === "string"){
+
+                if(!regexID.test(newId)){
+                    res.status(400)
+                    throw new Error("O novo id é inválido, o id deve ter a seguinte estrutura: 'a001', 'a123', 'a999'.")
+                }
+            }else{
+                res.status(400)
+                throw new Error("O novo id é inválido, ele deve ser do tipo 'string'.")
+            }
+           
+        }else if(newId === 0 || newId === ""){
+            throw new Error("O novo id é inválido, o id deve ter a seguinte estrutura: 'a001', 'a123', 'a999'.")
+        }
+
+        if(newOwnerName){
+            if(typeof(newOwnerName) === "string"){
+
+                if(!regexOwnerName.test(newOwnerName)){
+                    res.status(400)
+                    throw new Error("A propriedade 'ownerName' deve ter no mínimo 2 caracteres e precisa ser do tipo 'string'.")
+                }
+            }else{
+                res.status(400)
+                throw new Error("O novo 'ownerName' é inválido, ele deve ser do tipo 'string'.")
+            }
+        }else if(newOwnerName === 0 || newOwnerName === ""){
+            throw new Error("A propriedade 'ownerName' deve ter no mínimo 2 caracteres e precisa ser do tipo 'string'.")
+        }
+
+        if(newBalance !== null && newBalance !== undefined){
+
+            if(typeof(newBalance) !== "number" || newBalance < 0){
+                res.status(400)
+                throw new Error("O valor do balance deve ser númerico e maior ou igual a 0.")
+            }
+        }
+
+        if(newType){
+            
+            if(newType !== ACCOUNT_TYPE.BLACK && newType !== ACCOUNT_TYPE.GOLD && newType !== ACCOUNT_TYPE.PLATINUM){
+                throw new Error(`O accout type deve ser um desses itens ${JSON.stringify(ACCOUNT_TYPE).valueOf()}`)
+            }
+        }
+        
+
+        const account = accounts.find((account) => account.id === id) 
+
+        if (account) {
+            account.id = newId || account.id
+            account.ownerName = newOwnerName || account.ownerName
+            account.type = newType || account.type
+
+            account.balance = isNaN(newBalance) ? account.balance : newBalance
+        }
+
+        res.status(200).send(account)
+        } catch (error) {
+        
+            if(error instanceof Error){
+                res.send(error.message)
+            }
+    
+            res.status(500).send("Erro desconheicido!")
     }
-
-    res.status(200).send("Atualização realizada com sucesso")
+    
 })
